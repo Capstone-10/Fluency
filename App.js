ca51202d43cb4b876aabece903a1cc6907e69566
Peter Park <peterparkk25@gmail.com>
2021-07-08 12:50:09 -0400

    Feature: Tab (not working) Drawer, Stack Navigation

--- App.js
+++ App.js
@@ -1,32 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./screens/HomeScreen";
-import Camera from "./screens/Camera";
-import VoiceAndTextTranslate from "./screens/VoiceAndTextTranslate";
-import * as firebase from "firebase";
+import { DrawerNavigator } from "./Navigation/DrawerNavigator";
+// import { MainStackNavigator } from "./Navigation/StackNavigator";
+import BottomTabNavigator from "./Navigation/TabNavigator";
 
-var firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-};
-firebase.initializeApp(firebaseConfig);
-
-const Stack = createStackNavigator();
-
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
-        <Stack.Screen name="Camera" component={Camera} />
-        <Stack.Screen name="Translate" component={VoiceAndTextTranslate} />
-      </Stack.Navigator>
+      <DrawerNavigator />
+      {/* <MainStackNavigator /> */}
+      {/* <BottomTabNavigator /> */}
     </NavigationContainer>
   );
-}
+};
+
+export default App;
